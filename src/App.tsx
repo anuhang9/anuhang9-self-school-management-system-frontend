662158d9f48c966a8322e042060ac8388fa11f18
regex <regex@LAPTOP-6881BHQV.Home>
2025-07-06 16:42:28 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1 className='text-red-500'>anuhang</h1>
+    <Routes>
+      <Route path='/' element=""/>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
